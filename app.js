1e35263b81d7828d1afc33d529b86e180792d7e4
Rubén Ceballos <greimor@iMac-de-Ruben.local>
2019-08-04 23:32:19 +0200

    Bug imagenes

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images/', express.static(path.join(__dirname, '/images')));
 //app.use('/pdf', express.static(path.join(__dirname, '/mnt/sda3/images/pdfs')));
 
 /**
